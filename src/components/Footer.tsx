e72eb74e0ca66ab7efa13c7417db42eee7f4f20e
korchinmv <korchinmv@yandex.ru>
2024-03-01 15:16:30 +0300

    fix: footer mistake

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,10 @@ import Container from "./Container";
 
 const Footer: FC = () => {
   return (
-    <footer className="mt-auto bg-zinc-950 shadow-[--accent-color] shadow-[0px_0px_8px_3px]">
+    <footer className='mt-auto bg-zinc-950 shadow-[--accent-color] shadow-[0px_0px_8px_3px]'>
       <Container>
-        <p className="text-center text-[18px] md:text-[22px] py-[10px] md:py-[20px]">
-          Web-developer Korchin M.V. 2024 yaer
+        <p className='text-center text-[18px] md:text-[22px] py-[10px] md:py-[20px]'>
+          Web-developer Korchin M.V. 2024 year
         </p>
       </Container>
     </footer>
