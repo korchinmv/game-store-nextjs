be70b9b8c1315f9b6d0ec1fad09640de1a3fee4e
korchinmv <korchinmv@yandex.ru>
2024-03-06 09:49:40 +0300

    fixed: type Children in Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,4 +1,4 @@
-import { Children } from "@/types";
+import { Children } from "@/types/Children";
 
 const Container = ({ children }: Children) => {
   return <div className='container mx-auto px-4'>{children}</div>;
