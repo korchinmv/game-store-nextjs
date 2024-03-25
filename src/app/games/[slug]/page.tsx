5bf9e7a694c36cedfd58d0844dbc652f97de99cf
korchinmv <korchinmv@yandex.ru>
2024-03-25 17:03:46 +0300

    fixed: GameCard props

--- src/app/games/[slug]/page.tsx
+++ src/app/games/[slug]/page.tsx
@@ -246,15 +246,7 @@ const GamePage = ({ params: { slug } }: GamePageProps): ReactNode => {
 
             <ul className='grid grid-cols-1 gap-[10px] md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-[25px] mb-[20px]'>
               {dataGameDlc?.results.map((game: Game) => (
-                <GameCard
-                  key={game.id}
-                  id={game.id}
-                  name={game.name}
-                  price={game.playtime}
-                  rating={game.rating}
-                  bgImage={game.background_image}
-                  slug={game.slug}
-                />
+                <GameCard key={game.id} game={game} />
               ))}
             </ul>
           </>
